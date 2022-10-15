0e271a0e435db82e3a5524c45326468837d276f2
aldrin <aldrindl@careindeed.com>
2022-10-15 23:32:05 +0800

    index styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,16 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
       colors: {
         darkBlue: 'hsl(217, 28%, 15%)',
         darkBlue1: 'hsl(218, 28%, 13%)',
         darkBlue2: 'hsl(216, 53%, 9%)',
         darkBlue3: 'hsl(219, 30%, 18%)',
-        accentCyan: 'hsl(176, 68%, 64%)',
-        accentBlue: 'hsl(198, 60%, 50%)',
+        accentCyan: '#5271ff',
+        accentBlue: '#5271ff',
         lightRed: 'hsl(0, 100%, 63%)',
       }   
     },
